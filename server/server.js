a96525453763b3513a998457eed3df1d5ce5aa5d
Sina Pinto <sina.pinto@gmail.com>
2018-11-25 20:31:03 -0800

    split into client/ and server/

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const path = require( 'path' )
+const helmet = require('koa-helmet')
+const logger = require( 'koa-logger' )
+const mount = require( 'koa-mount' )
+const bodyparser = require( 'koa-bodyparser' )
+const debug = require('debug')('app')
+
+const PORT = process.env.PORT || 3001
+
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(helmet())
+app.use(logger())
+
+// store parsed body in `this.request.body`
+app.use(bodyparser({ enableTypes: ['json'] }))
+
+async function api(ctx) {
+    ctx.status = 200
+    ctx.type = 'json'
+    ctx.body = { ay: 'lmao' }
+}
+
+app.use(mount('/api', api))
+
+app.listen(PORT)
+debug(`${process.env.NODE_ENV} server listening at http://localhost:${PORT}`)
