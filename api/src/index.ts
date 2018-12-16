15c317ea7095c88ccb51aeab554f918de4a94907
Sina Pinto <sina.pinto@gmail.com>
2018-12-16 13:13:38 -0800

    setup typeorm connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,29 +2,44 @@ import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
 import * as express from 'express'
 import { buildSchema } from 'type-graphql'
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { createConnection } from 'typeorm'
+import { User } from './entity/User'
 
-;(async () => {
-  let config = await getConnectionOptions(process.env.NODE_ENV)
-  await createConnection({
-    ...config,
-    name: 'default',
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'dev',
+  password: 'dev',
+  database: 'microblog2',
+  entities: [User],
+  logging: true,
+  synchronize: true,
+})
+  .then(async connection => {
+    let user = new User()
+    user.firstName = 'Timber'
+    user.lastName = 'Saw'
+    user.age = 25
+    await connection.manager.save(user)
+
+    let app = express()
+
+    let server = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [],
+      }),
+      context: ({ req }: any) => ({
+        req,
+      }),
+    })
+
+    server.applyMiddleware({ app, cors: false })
+
+    app.listen({ port: 4000 }, () =>
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      )
+    )
   })
-
-  let app = express()
-
-  let server = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [],
-    }),
-    context: ({ req }: any) => ({
-      req,
-    }),
-  })
-
-  server.applyMiddleware({ app, cors: false })
-
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  )
-})()
+  .catch(console.error)
