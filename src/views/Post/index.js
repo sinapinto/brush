432070511e6101fd848b84e487cbd56758e7c1e4
Sina Pinto <sina.pinto@gmail.com>
2018-12-18 21:12:03 -0800

    remove unused code

--- src/views/Post/index.js
+++ src/views/Post/index.js
@@ -1,22 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { H2, P, Card } from '../../components/globals'
-import { getPost } from '../../fetch/post'
-import usePosts from '../../hooks/usePosts'
+import { H2, Card } from '../../components/globals'
 
 export default function Post({ id }) {
-  let [post = {}, setPost] = usePosts(id)
-
-  useEffect(() => {
-    getPost(id)
-      .then(post => setPost(post))
-      .catch(console.error)
-  }, [])
-
   return (
     <Card>
-      <H2>post {id}</H2>
-      <P>{post.body}</P>
+      <H2>post</H2>
     </Card>
   )
 }
