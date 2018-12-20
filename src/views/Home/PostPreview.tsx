2473c7e5caa4e5e1f83a0d90bc0019a0d9cd5aa7
Sina Pinto <sina.pinto@gmail.com>
2018-12-20 13:37:40 -0800

    migrate to tsx

--- src/views/Home/PostPreview.tsx
+++ src/views/Home/PostPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { H3, P } from '../../components/globals';
@@ -8,7 +7,15 @@ let Container = styled.div`
   padding: 16px;
 `;
 
-export default function PostPreview({ id, title, author }) {
+type Props = {
+  id: string;
+  title?: string;
+  author?: {
+    username: string;
+  };
+};
+
+export default function PostPreview({ id, title, author }: Props) {
   return (
     <Container>
       <H3>
@@ -22,11 +29,3 @@ export default function PostPreview({ id, title, author }) {
     </Container>
   );
 }
-
-PostPreview.propTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string,
-  author: PropTypes.shape({
-    username: PropTypes.string,
-  }),
-};
