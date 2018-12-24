1f90438574735e326956cc7a772f37f9b8c46fc2
Sina Pinto <sina.pinto@gmail.com>
2018-12-23 19:53:21 -0800

    check if current user is author

--- src/views/Post/index.tsx
+++ src/views/Post/index.tsx
@@ -6,36 +6,53 @@ import styled from 'styled-components';
 import TextEditor from '../../components/TextEditor';
 import { H2i, P, Card, BlankSlate } from '../../components/globals';
 import { getPostByIdQuery } from '../../graphql/queries/post';
+import { currentUserQuery } from '../../graphql/queries/user';
 import DeleteButton from './DeleteButton';
+import {
+  GetPostById,
+  GetPostByIdVariables,
+} from '../../graphql/queries/__generated__/GetPostById';
+import { CurrentUser } from '../../graphql/queries/__generated__/CurrentUser';
 
 type PostProps = {
   id: string;
 };
 
+class GetPostByIdQuery extends Query<GetPostById, GetPostByIdVariables> {}
+class CurrentUserQuery extends Query<CurrentUser> {}
+
 const Post: React.FunctionComponent<PostProps> = ({ id }) => {
   return (
     <Card>
-      <Query query={getPostByIdQuery} variables={{ id }}>
-        {({ data, loading, error }) => {
-          if (loading) return 'Loading...';
-          if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-          const post = data.getPost;
-          if (!post) {
-            return <BlankSlate>That post doesn't exist!</BlankSlate>;
-          }
-          const editorValue = Value.fromJSON(JSON.parse(post.body));
-          return (
-            <>
-              <HeaderWrap>
-                <H2i>{post.title}</H2i>
-                <DeleteButton postId={post.id} />
-              </HeaderWrap>
-              <TextEditor readOnly value={editorValue} />
-              <P>By {post.author.username}</P>
-            </>
-          );
-        }}
-      </Query>
+      <CurrentUserQuery query={currentUserQuery} errorPolicy="ignore">
+        {({ data: currentUserData }) => (
+          <GetPostByIdQuery query={getPostByIdQuery} variables={{ id }}>
+            {({ data, loading, error }) => {
+              if (loading) return 'Loading...';
+              if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+              if (!data || !data.getPost) {
+                return <BlankSlate>That post doesn't exist!</BlankSlate>;
+              }
+              const post = data.getPost;
+              const editorValue = Value.fromJSON(JSON.parse(post.body));
+              const isAuthor =
+                currentUserData &&
+                currentUserData.currentUser &&
+                currentUserData.currentUser.id === post.author.id;
+              return (
+                <>
+                  <HeaderWrap>
+                    <H2i>{post.title}</H2i>
+                    {isAuthor && <DeleteButton postId={post.id} />}
+                  </HeaderWrap>
+                  <TextEditor readOnly value={editorValue} />
+                  <P>By {post.author.username}</P>
+                </>
+              );
+            }}
+          </GetPostByIdQuery>
+        )}
+      </CurrentUserQuery>
     </Card>
   );
 };
