6d12346f65d79d1dc216dd4124292d2f25b14f8a
Sina Pinto <sina.pinto@gmail.com>
2019-01-11 17:47:14 -0800

    same-site: strict

--- api/src/index.ts
+++ api/src/index.ts
@@ -42,7 +42,7 @@ createConnection({
         saveUninitialized: false,
         proxy: true,
         cookie: {
-          sameSite: 'lax',
+          sameSite: 'strict',
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
           maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
