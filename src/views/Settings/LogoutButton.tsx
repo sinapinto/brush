9ccae5b6b3e81f6571b61b43369bb059fa89c7c8
Sina Pinto <sina.pinto@gmail.com>
2019-01-14 21:09:21 -0800

    fix error when logging out

--- src/views/Settings/LogoutButton.tsx
+++ src/views/Settings/LogoutButton.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
+import { RouterProps, withRouter } from 'react-router';
 import { Button } from '../../components/Button';
 import { logoutUserMutation as MUTATION } from '../../graphql/mutations/user';
 import { LogoutUser } from '../../graphql/mutations/__generated__/LogoutUser';
 
 class LogoutUserMutation extends Mutation<LogoutUser> {}
 
-export const LogoutButton = () => {
+export const LogoutButton = withRouter(({ history }: RouterProps) => {
   return (
     <LogoutUserMutation mutation={MUTATION}>
       {(mutate, { client }) => (
         <Button
           type="button"
-          onClick={() => mutate().then(() => client.resetStore())}
+          onClick={() =>
+            mutate().then(() => {
+              client.resetStore();
+              history.push('/');
+            })
+          }
         >
           Log Out
         </Button>
       )}
     </LogoutUserMutation>
   );
-};
+});
